db48517ea28e85ba01ee3c75a099e5eb2808e3fa
ddh <zhangsna@11.com>
2019-08-02 15:17:18 +0800

    practice demo init

---
+++ practice-demo/src/main/java/com/ddh/practicedemo/PracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ddh.practicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeDemoApplication.class, args);
+    }
+
+}
