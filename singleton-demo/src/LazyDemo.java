f873bf73aad19911e13d83b5b89a33acd9b46b6c
ddh <zhangsna@11.com>
2019-08-02 11:50:42 +0800

    singleton init

---
+++ singleton-demo/src/LazyDemo.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @author ddh
+ * @date 2019/8/2 11:42
+ * @description 单例模式 懒汉式
+ **/
+public class LazyDemo {
+
+
+    @Test
+    public void test01(){
+
+    }
+}
