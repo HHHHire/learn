040442646d00ccf928a0fd9ed7dea4a13c53a2e8
ddh <dengdh167@163.com>
2019-08-23 20:39:02 +0800

    second

--- thread-pool/src/learn/Demo2.java
+++ thread-pool/src/learn/Demo2.java
@@ -1,10 +1,28 @@
 package learn;
 
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author ddh
  * @date 2019/8/23 20:23
  * @description
  **/
 public class Demo2 {
-
+    @Test
+    public void test01() throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            int n = i;
+            Thread.sleep(1000);
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(n);
+                }
+            });
+        }
+    }
 }
