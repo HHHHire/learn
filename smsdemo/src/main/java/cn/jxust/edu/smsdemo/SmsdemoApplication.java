8abaa3be646607c3a6412d429679c8577144057a
HHHHire <dengdh167@163.com>
2019-09-20 20:43:26 +0800

    smsdemo init

---
+++ smsdemo/src/main/java/cn/jxust/edu/smsdemo/SmsdemoApplication.java
@@ -0,0 +1,13 @@
+package cn.jxust.edu.smsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsdemoApplication.class, args);
+    }
+
+}
