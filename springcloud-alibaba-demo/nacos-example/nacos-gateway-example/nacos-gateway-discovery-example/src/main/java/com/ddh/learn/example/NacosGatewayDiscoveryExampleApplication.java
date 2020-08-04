68cc0b05654d5ef97300302439c47c0119a449f7
HHHHire <dengdh167@163.com>
2020-08-04 14:05:39 +0800

    test

---
+++ springcloud-alibaba-demo/nacos-example/nacos-gateway-example/nacos-gateway-discovery-example/src/main/java/com/ddh/learn/example/NacosGatewayDiscoveryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ddh.learn.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosGatewayDiscoveryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayDiscoveryExampleApplication.class, args);
+    }
+
+}
---
+++ springcloud-alibaba-demo/nacos-example/nacos-gateway-example/nacos-gateway-provider-example/src/main/java/com/ddh/learn/example/NacosGatewayProviderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ddh.learn.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosGatewayProviderExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayProviderExampleApplication.class, args);
+    }
+
+}
