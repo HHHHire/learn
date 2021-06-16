258b3436ba56e62845df09444527261dee26fa61
dengdh <dengdh167@163.com>
2021-06-16 23:21:46 +0800

    ...

--- java/web-http-demo/netty-demo/src/main/java/com/ddh/learn/netty/DemoTest.java
+++ java/web-http-demo/netty-demo/src/main/java/com/ddh/learn/netty/DemoTest.java
@@ -5,5 +5,19 @@ package com.ddh.learn.netty;
  * @data: 2021/5/8 10:08
  */
 public class DemoTest {
+    public void run1() {
 
+    }
+
+    public void run2() {
+
+    }
+
+    public void testException() {
+        try {
+            run1();
+        } finally {
+            run2();
+        }
+    }
 }
