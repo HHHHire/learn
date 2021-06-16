175b1bdbaae69113e723f2e251de50765f52afbf
dengdh <dengdh167@163.com>
2021-06-16 23:26:42 +0800

    reactive

--- reactive-learn/reactive-demo/src/main/java/com/ddh/learn/reactive/NumberPublisher.java
+++ reactive-learn/reactive-demo/src/main/java/com/ddh/learn/reactive/NumberPublisher.java
@@ -1,9 +1,45 @@
 package com.ddh.learn.reactive;
 
+import javafx.util.Pair;
+
+import java.util.concurrent.*;
+
 /**
  * @author: dengdh@dist.com.cn
  * @date: 2021/6/2 16:47
  * @description:
  */
-public class NumberPublisher {
+public class NumberPublisher implements Flow.Publisher<Pair<Integer, Integer>> {
+    private int a;
+    private int b;
+
+    private BlockingQueue<Pair<Integer, Integer>> queue = new LinkedBlockingQueue<>();
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super Pair<Integer, Integer>> subscriber) {
+
+    }
+
+    class NumberSubscription implements Flow.Subscription {
+        private final ExecutorService executor = ForkJoinPool.commonPool();
+        private final Flow.Subscriber<? super Pair<Integer, Integer>> subscriber;
+
+        public NumberSubscription(Flow.Subscriber<? super Pair<Integer, Integer>> subscriber) {
+            this.subscriber = subscriber;
+        }
+
+        @Override
+        public void request(long n) {
+            for (int i = 0; i < n; i++) {
+                executor.submit(() -> {
+                    subscriber.onNext(queue.poll());
+                });
+            }
+        }
+
+        @Override
+        public void cancel() {
+            executor.shutdown();
+        }
+    }
 }
--- reactive-learn/reactive-demo/src/main/java/com/ddh/learn/reactive/NumberSubscriber.java
+++ reactive-learn/reactive-demo/src/main/java/com/ddh/learn/reactive/NumberSubscriber.java
@@ -1,9 +1,45 @@
 package com.ddh.learn.reactive;
 
+import cn.hutool.core.util.StrUtil;
+import javafx.util.Pair;
+
+import java.util.concurrent.Flow;
+
 /**
  * @author: dengdh@dist.com.cn
  * @date: 2021/6/2 16:39
  * @description:
  */
-public class NumberSubscriber {
+public class NumberSubscriber implements Flow.Subscriber<Pair<Integer, Integer>> {
+
+    private Flow.Subscription subscription;
+
+    private int c;
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        this.subscription.request(1);
+        System.out.println(StrUtil.format("ThreadName: {}, {}",
+                Thread.currentThread().getName(), subscription));
+    }
+
+    @Override
+    public void onNext(Pair<Integer, Integer> item) {
+        Integer a = item.getKey();
+        Integer b = item.getValue();
+        this.c = a + b;
+        System.out.println(StrUtil.format("ThreadName: {}, {}, {}", Thread.currentThread().getName(),
+                item, this.c));
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println(StrUtil.format("ThreadName: {}, 结束了", Thread.currentThread().getName()));
+    }
 }
